4688ade255fa0eb136c63a0c03b9e99a6f38783d
Christian Quadri <christian.quadri@unimi.it>
2023-03-20 17:12:45 +0100

    fixed unused includes

--- src/carla_omnet/TodCarlanetManager.cc
+++ src/carla_omnet/TodCarlanetManager.cc
@@ -1,18 +1,5 @@
 #include "TodCarlanetManager.h"
 
-#include <stdexcept>
-
-#include "inet/applications/base/ApplicationPacket_m.h"
-#include "inet/common/ModuleAccess.h"
-#include "inet/common/TagBase_m.h"
-#include "inet/common/TimeTag_m.h"
-#include "inet/common/lifecycle/ModuleOperations.h"
-#include "inet/common/packet/Packet.h"
-#include "inet/networklayer/common/FragmentationTag_m.h"
-#include "inet/networklayer/common/L3AddressResolver.h"
-#include "inet/transportlayer/contract/udp/UdpControlInfo_m.h"
-
-#include "app/TODCarApp.h"
 
 using namespace inet;
 using namespace std;
