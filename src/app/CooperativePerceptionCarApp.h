55bd031f5be01ec1c0af3919016ff622cb5d5f11
Stefano di Terlizzi <stefanoditerlizzi96@gmail.com>
2024-10-26 12:26:29 +0200

    not working cooperative perception app + minor fix
    a not working cooperative perception app module + refactoring of
    Todcarapp with Inheritance from cooperative perception app

---
+++ src/app/CooperativePerceptionCarApp.h
@@ -0,0 +1,100 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __TOD_OMNET_COOPERATIVEPERCEPTIONCARAPP_H_
+#define __TOD_OMNET_COOPERATIVEPERCEPTIONCARAPP_H_
+
+#include <vector>
+#include <omnetpp.h>
+
+#include "../carla_omnet/TodCarlanetManager.h"
+#include "inet/networklayer/common/L3Address.h"
+#include "inet/transportlayer/contract/udp/UdpSocket.h"
+#include "inet/applications/base/ApplicationBase.h"
+
+#include "messages/TodMessages_m.h"
+
+using namespace omnetpp;
+using namespace inet;
+
+/**
+ * TODO - Generated class
+ */
+class CooperativePerceptionCarApp : public ApplicationBase, public UdpSocket::ICallback
+{
+  private:
+    TodCarlanetManager* carlaCommunicationManager;
+    cMessage* updateStatusSelfMessageCoop;
+    double statusUpdateIntervalCoop;
+    const char *actorId;
+    const int CREATION_STATUS_DATA_MSG_KIND_COOP = 3;
+
+    const int PROCESS_STATUS_MESSAGE_KIND = 1;
+    map<pair<string,string>, int> reassembleStatusPacketsMap;
+
+
+  protected:
+    UdpSocket socket;
+    L3Address destAddressesCoop;
+    int destPort;
+
+
+  protected:
+    //virtual void initialize() override;
+    //virtual void handleMessage(cMessage *msg) override;
+    virtual int numInitStages() const override { return inet::NUM_INIT_STAGES; }
+    virtual void initialize(int stage) override;
+    virtual void handleMessageWhenUp(cMessage *msg) override;
+    virtual void finish() override;
+    virtual void refreshDisplay() const override;
+
+    virtual void handleStartOperation(LifecycleOperation *operation) override;
+    virtual void handleStopOperation(LifecycleOperation *operation) override;
+    virtual void handleCrashOperation(LifecycleOperation *operation) override;
+
+
+    virtual void retrieveStatusDataCoop();
+    /*Application logic*/
+    virtual void sendUpdateStatusPacketCoop(simtime_t dataRetrievalTime);
+
+    /*UDP logic*/
+    /**
+     * Notifies about data arrival, packet ownership is transferred to the callee.
+     */
+    virtual void socketDataArrived(UdpSocket *socket, Packet *packet);
+
+    /**
+     * Notifies about error indication arrival, indication ownership is transferred to the callee.
+     */
+    virtual void socketErrorArrived(UdpSocket *socket, Indication *indication);
+
+    /**
+     * Notifies about socket closed, indication ownership is transferred to the callee.
+     */
+    virtual void socketClosed(UdpSocket *socket);
+
+
+    virtual void sendPacket(Packet *pk);
+    virtual void processPacket(Packet *pk);
+
+    virtual void handleStatusUpdateMessage(Packet *statusPacket);
+
+    virtual bool reassembleStatusPacket(string actorId, string statusId, int numFragments);
+
+public:
+    ~CooperativePerceptionCarApp();
+};
+
+#endif
--- src/carla_omnet/TodCarlanetManager.cc
+++ src/carla_omnet/TodCarlanetManager.cc
@@ -8,10 +8,14 @@ Define_Module(TodCarlanetManager);
 
 
 
-const map<string,cValue>& TodCarlanetManager::getExtraInitParams(){
+/*const map<string,cValue>& TodCarlanetManager::getExtraInitParams(){
     auto extraInitParams = new cValueMap();
     extraInitParams->set("carla_world_configuration",  cValue(par("carlaConfiguration").stdstringValue()));
     return extraInitParams->getFields();
+}*/
+
+const std::map<std::string,cValue>& TodCarlanetManager::getExtraInitParams(){
+    return check_and_cast<cValueMap*>(par("extraInitParams").objectValue())->getFields();
 }
 
 
@@ -20,11 +24,10 @@ const map<string,cValue>& TodCarlanetManager::getExtraInitParams(){
  * */
 
 string TodCarlanetManager::getActorStatus(string actorId){
-    EV_INFO << "Contact Carla for getting the status id" << endl;
+    EV_INFO << "Contact Carla for getting the status id: "<< actorId << endl;
     tod_carla_api::actor_status_update requestMsg;
     requestMsg.actor_id = actorId;
 
-    //tod_carla_api::actor_status response = sendToAndGetFromCarla<tod_carla_api::actor_status_update, tod_carla_api::actor_status>(requestMsg);
     tod_carla_api::actor_status response = sendToAndGetFromCarla_actor_generic_message<tod_carla_api::actor_status_update, tod_carla_api::actor_status>(requestMsg);
 
 
@@ -40,7 +43,6 @@ string TodCarlanetManager::computeInstruction(string actorId, string statusId, s
     requestMsg.status_id = statusId;
 
     //json j = requestMsg;
-    //tod_carla_api::instruction response = sendToAndGetFromCarla<tod_carla_api::compute_instruction,tod_carla_api::instruction>(requestMsg);
     tod_carla_api::instruction response = sendToAndGetFromCarla_agent_generic_message<tod_carla_api::compute_instruction,tod_carla_api::instruction>(requestMsg);
 
     return response.instruction_id;
@@ -51,11 +53,25 @@ void TodCarlanetManager::applyInstruction(string actorId, string instructionId){
     tod_carla_api::apply_instruction requestMsg;
     requestMsg.actor_id = actorId;
     requestMsg.instruction_id = instructionId;
-    
-    //sendToAndGetFromCarla<tod_carla_api::apply_instruction, tod_carla_api::ok>(requestMsg);
+
     sendToAndGetFromCarla_actor_generic_message<tod_carla_api::apply_instruction, tod_carla_api::ok>(requestMsg);
 
 
 }
 
+void TodCarlanetManager::sendStatusToActor(string actorId, string statusId){
 
+    //TODO: implement this following zerodelay network COOPERATIVE_UPDATE.json
+    EV_INFO << "send to Carla for getting the instruction id" << endl;
+
+    tod_carla_api::cooperative_update requestMsg;
+    requestMsg.actor_id = actorId;
+    requestMsg.status_id = statusId;
+
+    sendToAndGetFromCarla_actor_generic_message<tod_carla_api::cooperative_update,tod_carla_api::ok>(requestMsg);
+}
+
+
+void CarlanetManager::finish() {
+    std::cout << "finish(), before end the simulation" << endl;
+}
